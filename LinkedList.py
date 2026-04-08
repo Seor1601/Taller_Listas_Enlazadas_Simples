4e1f1422b521b9bbda801cde8d23ef8e18898e95
MariTZ25 <mariactz2008@gmail.com>
2026-04-08 16:54:09 +0000

    4,5,6

---
+++ LinkedList.py
@@ -0,0 +1,65 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def display (self):
+        current = self.head
+        while current is not None:
+            print(current.data, end= "-->")
+            current = current.next
+            print("None")
+
+    def search(self, target):
+        current = self.head
+
+        while current is not None:
+          if current.data == target:
+                return current
+          current = current.next
+          return -1
+    
+    def delete_head(self):
+        if self.head is None:
+            print("No hay elementos que eliminar")
+        if self.head is not None:
+            current= self.head
+            while current.next is not None:
+                self.head=current.next
+            current=None
+        return
+    
+    def deleteXsearch(self, data):
+        current = self.head.data
+        while current is not None:
+          if current.data == data:
+            while current.next is not None:
+                current=current.next
+            current = current.next
+            current=None
+          
+          
+        
+
+
+
+    def delete_end(self):
+        if self.head is None:
+            print("No hay elementos que eliminar")
+
+        current = self.head
+        while current.next is None:
+            current = None
+        while current.next is not None:
+            current = current.next
+        current.next = None
+        return
+
+        
+             
+            
+
+        
